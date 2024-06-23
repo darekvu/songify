bca638d84247d20b7c80fca9c77c6baf4fef575d
Quang Du Vu (Darek) <vuquang.dd@gmail.com>
2024-06-23 20:54:09 +0200

    object mapper vs  jsonPath in get/songs controller testing

--- src/integration/java/feature/HappyPathIntegrationTest.java
+++ src/integration/java/feature/HappyPathIntegrationTest.java
@@ -1,7 +1,10 @@
 package feature;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.songify.SongifyApplication;
+import com.songify.infrastructure.crud.song.controller.dto.response.GetAllSongsResponseDto;
+import com.songify.infrastructure.crud.song.controller.dto.response.SongControllerDto;
 import org.junit.jupiter.api.Test;
 import org.springdoc.webmvc.core.service.RequestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,15 +15,22 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.empty;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = SongifyApplication.class)
 @Testcontainers
@@ -33,8 +43,10 @@ class HappyPathIntegrationTest {
             .withDatabaseName("testing");
     @Autowired
     private MockMvc mockMvc;
+
     @Autowired
-    private RequestService requestBuilder;
+    public ObjectMapper objectMapper;
+
 
     @DynamicPropertySource
     public static void propertyOverride(DynamicPropertyRegistry registry){
@@ -43,16 +55,23 @@ class HappyPathIntegrationTest {
 
     @Test
     void canStartPostgresDB() {
-        //given
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isCreated()).isTrue();
-        //when
-        //then
     }
 
     @Test
     void f() throws Exception {
-        mockMvc.perform(get("/songs")
+//        when go to /songs then there no songs returned
+        ResultActions perform = mockMvc.perform(get("/songs")
                 .contentType(MediaType.APPLICATION_JSON));
+        MvcResult getSongsActionResult = perform.andExpect(status().isOk()).andReturn();
+        String contentAsString = getSongsActionResult.getResponse().getContentAsString();
+        GetAllSongsResponseDto allSongsResponseDto = objectMapper.readValue(contentAsString, GetAllSongsResponseDto.class);
+        List<SongControllerDto> songs = allSongsResponseDto.songs();
+        assertThat(songs.isEmpty());
+//         mockMvc.perform(get("/songs")
+//                .contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.songs",empty()));
     }
 }
